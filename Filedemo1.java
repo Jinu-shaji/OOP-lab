331268966cb3ee299d9c695a34c036fbb21888dd
JINU <20mp1604@rit.ac.in>
2021-09-25 14:24:06 +0530

    program 5

---
+++ Filedemo1.java
@@ -0,0 +1,17 @@
+
+import java.io.File;
+
+class Filedemo1 {
+  public static void main(String[] args) {
+
+    // creates a file object
+    File file = new File("/home/user");
+
+    // returns an array of all files
+    String[] fileList = file.list();
+
+    for(String str : fileList) {
+      System.out.println(str);
+    }
+  }
+}
